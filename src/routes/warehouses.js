b732e5cb6b26bfe84217f3d4d752524c07a87165
alper <alperzini@hotmail.com>
2026-02-07 08:36:15 -0800

    GET Single warehouse API

--- src/routes/warehouses.js
+++ src/routes/warehouses.js
@@ -18,10 +18,42 @@ export const getAllWarehouses = async (req, res) => {
 
 router.get("/", getAllWarehouses);
 
+// GET /warehouses/:id
 
+export const getWarehouseById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const [rows] = await db.query("SELECT * FROM warehouses WHERE id = ?", [id]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ message: "Event not found" });
+        }
+        res.json(rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Failed to fetch warehouse "})
+    }
+}
+
+router.get("/:id", getWarehouseById);
 
 
 
 export default router;
+
+/* API Testing
+GET All the warehouses:
+http://localhost:8080/warehouses
+
+GET single warehouse:
+http://localhost:8080/warehouses/1
+http://localhost:8080/warehouses/5
+http://localhost:8080/warehouses/999 //returns 404
+
+
+
+
+
+*/
 
 
