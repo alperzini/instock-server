a3082acaf6992fd0749ced4cf643c18b37ac6463
Shyine Makin <shyine@Shyines-MacBook-Pro.local>
2026-02-21 12:31:49 -0500

    updated with main

--- src/db/db.js
+++ src/db/db.js
@@ -18,6 +18,9 @@ import "dotenv/config";
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
+
+    //createPool handles multiple simultaneous API requests at once, opens multiple connections, pool manages to reconnect logic for us
+
 host: process.env.DB_HOST || "127.0.0.1",
 user: process.env.DB_USER,
 password: process.env.DB_PASSWORD,
